a90795e246973a0b6b8872d3e49b0ecf451c367b
obinnadtq <oisiwekpeni@gmail.com>
2020-06-26 22:09:40 +0200

    added code for plotting

--- AmplifyAndForward.py
+++ AmplifyAndForward.py
@@ -1,5 +1,6 @@
 # Import the necessary modules
 import numpy as np
+import matplotlib.pyplot as plt
 
 # SNR in dB
 SNR_db_1 = 3
@@ -78,73 +79,84 @@ p_x_given_y = p_y_given_x * np.tile(np.expand_dims(np.tile(p_x, Ny // Nx) / p_y,
 # Amplify and Forward
 
 # SNR for second channel in sensor in dB
-SNR_db_2 = 10
+SNR_db_2 = [2, 4, 6, 8, 10, 12]
 
-# SNR in linear scale
-SNR_lin_2 = 10 ** (SNR_db_2 / 10)
+IXZ = []
 
-# variance of signal into the sensor
-sigma2_Y = 1
+for idx in range(len(SNR_db_2)):
+    # SNR in linear scale
+    SNR_lin_2 = 10 ** (SNR_db_2[idx] / 10)
 
-# variance of noise in sensor
-sigma2_N_sensor = sigma2_Y / SNR_lin_2
+    # variance of signal into the sensor
+    sigma2_Y = 1
 
-# standard deviation of each part of the complex noise
-sigma_N_2_sensor = np.sqrt(sigma2_N_sensor / 2)
+    # variance of noise in sensor
+    sigma2_N_sensor = sigma2_Y / SNR_lin_2
 
-# AWGN for Amplify and Forward sensing
-n_2 = (np.random.randn(np.size(y)) + 1j * np.random.randn(np.size(y))) * sigma_N_2_sensor
+    # standard deviation of each part of the complex noise
+    sigma_N_2_sensor = np.sqrt(sigma2_N_sensor / 2)
 
-# sampling interval
-dz = 2 * (np.amax(y) + 5 * np.sqrt(sigma2_N_sensor)) / Nz
+    # AWGN for Amplify and Forward sensing
+    n_2 = (np.random.randn(np.size(y)) + 1j * np.random.randn(np.size(y))) * sigma_N_2_sensor
 
-# discretized z
-z = np.arange(-(np.amax(y) + 5 * np.sqrt(sigma2_N_sensor)), (np.amax(y) + 5 * np.sqrt(sigma2_N_sensor)), dz).reshape(
-    Nz, 1)
+    # sampling interval
+    dz = 2 * (np.amax(y) + 5 * np.sqrt(sigma2_N_sensor)) / Nz
 
-# initialized p(z|y)
-p_z_y = np.ones((Nz, Ny))
+    # discretized z
+    z = np.arange(-(np.amax(y) + 5 * np.sqrt(sigma2_N_sensor)), (np.amax(y) + 5 * np.sqrt(sigma2_N_sensor)), dz).reshape(
+        Nz, 1)
 
-# initialize the temporary matrix to store conditional pdf
-temp1 = np.zeros((Nz, 1))
+    # initialized p(z|y)
+    p_z_y = np.ones((Nz, Ny))
 
-# obtain the conditional pdf p(y|x) for a Gaussian channel
-for idx in range(0, Ny):
-    tmp1 = (1 / np.sqrt(2 * np.pi * sigma2_N_sensor)) * np.exp(-(z - y[idx]) ** 2 / (2 * sigma2_N_sensor))
-    temp1 = np.append(temp1, tmp1, axis=1)
+    # initialize the temporary matrix to store conditional pdf
+    temp1 = np.zeros((Nz, 1))
 
-# remove the zero from the first column of the matrix
-pz_remove_initial_zeroes = temp1[:, -Ny:]
+    # obtain the conditional pdf p(y|x) for a Gaussian channel
+    for idx in range(0, Ny):
+        tmp1 = (1 / np.sqrt(2 * np.pi * sigma2_N_sensor)) * np.exp(-(z - y[idx]) ** 2 / (2 * sigma2_N_sensor))
+        temp1 = np.append(temp1, tmp1, axis=1)
 
-# obtain the conditional pdf
-p_z_given_y = p_z_y * pz_remove_initial_zeroes
+    # remove the zero from the first column of the matrix
+    pz_remove_initial_zeroes = temp1[:, -Ny:]
 
-# normalization for the pdf
-norm_sum_1 = np.sum(p_z_given_y, 0)
+    # obtain the conditional pdf
+    p_z_given_y = p_z_y * pz_remove_initial_zeroes
 
-# p(z|y)
-p_z_given_y = p_z_given_y / np.tile(norm_sum_1, (Nz, 1))
+    # normalization for the pdf
+    norm_sum_1 = np.sum(p_z_given_y, 0)
 
-# p(z)
-p_z = np.sum(p_z_given_y * p_y, 1)
+    # p(z|y)
+    p_z_given_y = p_z_given_y / np.tile(norm_sum_1, (Nz, 1))
 
-# p(z|y) expanded
-p_z_given_y_expanded = np.tile(np.expand_dims(p_z_given_y, axis=2), (1, 1, Nx))
+    # p(z)
+    p_z = np.sum(p_z_given_y * p_y, 1)
 
-g = np.sum(np.tile(np.expand_dims(p_x_y, axis=0), (Nz, 1, 1)) * p_z_given_y_expanded, 1)
+    # p(z|y) expanded
+    p_z_given_y_expanded = np.tile(np.expand_dims(p_z_given_y, axis=2), (1, 1, Nx))
 
-# p(z|y)
-p_x_given_z = 1/np.tile(np.expand_dims(p_z + 1e-31, axis=1), (1, Nx)) * g
+    g = np.sum(np.tile(np.expand_dims(p_x_y, axis=0), (Nz, 1, 1)) * p_z_given_y_expanded, 1)
 
-# p(x,y)
-p_x_z = p_x_given_z * np.tile(np.expand_dims(p_z, axis=1), (1, Nx))
+    # p(z|y)
+    p_x_given_z = 1/np.tile(np.expand_dims(p_z + 1e-31, axis=1), (1, Nx)) * g
 
-p_x_times_p_z = np.tile(np.expand_dims(p_x, axis=0), (Nz, 1)) * np.tile(np.expand_dims(p_z, axis=1), (1, Nx))
+    # p(x,y)
+    p_x_z = p_x_given_z * np.tile(np.expand_dims(p_z, axis=1), (1, Nx))
 
-w1 = np.log(p_x_z + 1e-31) - np.log(p_x_times_p_z + 1e-31)
+    p_x_times_p_z = np.tile(np.expand_dims(p_x, axis=0), (Nz, 1)) * np.tile(np.expand_dims(p_z, axis=1), (1, Nx))
 
-# I(X;Z)
-I_x_z = np.sum(p_x_z * w1)
+    w1 = np.log(p_x_z + 1e-31) - np.log(p_x_times_p_z + 1e-31)
+
+    # I(X;Z)
+    I_x_z = np.sum(p_x_z * w1)
+
+    IXZ.append(I_x_z)
+
+plt.plot(SNR_db_2, IXZ, 'b+-', linewidth=2)
+plt.title('Relevant Information versus SNR')
+plt.xlabel('SNR in dB')
+plt.ylabel('I(X;Z)')
+plt.show()
 
 
 
